02249a38512732638749e7dd3fee3cf72894ff76
Adam Driscoll <adam.r.driscoll@gmail.com>
2020-09-03 11:56:07 -0600

    Workflows.

---
+++ vscode.build.ps1
@@ -0,0 +1,24 @@
+task BuildExtension {
+    & {
+        $ErrorActionPreference = 'SilentlyContinue'
+        npm install -g npm
+        npm install -g typescript@latest
+        npm install -g vsce
+        npm install
+
+        Remove-Item (Join-Path $PSScriptRoot "kit") -Force -Recurse -ErrorAction SilentlyContinue
+        New-Item (Join-Path $PSScriptRoot "kit") -ItemType Directory
+
+        vsce package
+
+        Copy-Item (Join-Path $PSScriptRoot "*.vsix") (Join-Path $PSScriptRoot "kit")
+    }
+}
+
+task PublishExtension {
+    npm install -g vsce
+    $vsix = (Get-ChildItem "$PSScriptRoot-*.vsix").FullName
+    vsce publish --packagePath $vsix -p $env:GalleryToken
+}
+
+task . BuildExtension
