474aebcede2d760c3da2acbd341db1cec1c754f6
Adam Driscoll <adam.r.driscoll@gmail.com>
2021-09-14 20:25:57 -0600

    Fix build.

--- vscode.build.ps1
+++ vscode.build.ps1
@@ -6,12 +6,14 @@ task BuildExtension {
         npm install -g vsce
         npm install
 
+        Remove-Item (Join-Path $PSScriptRoot "out") -Force -Recurse -ErrorAction SilentlyContinue
+        New-Item (Join-Path $PSScriptRoot "out") -ItemType Directory
         Remove-Item (Join-Path $PSScriptRoot "kit") -Force -Recurse -ErrorAction SilentlyContinue
         New-Item (Join-Path $PSScriptRoot "kit") -ItemType Directory
         Copy-Item "$PSScriptRoot\src\Universal.VSCode.psm1" "$PSScriptRoot\out"
 
         vsce package
-
+        
         Copy-Item (Join-Path $PSScriptRoot "*.vsix") (Join-Path $PSScriptRoot "kit")
     }
 }
